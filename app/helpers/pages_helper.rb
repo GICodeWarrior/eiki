1c33e2022d8f32d18cac23c6e4318226907b04c3
Rusty Burchfield <rusty@gicodelt.csh.rit.edu>
2009-01-18 11:52:58 -0800

    Refactor rendering helper.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,11 +2,7 @@ module PagesHelper
   def render_body(page)
     body = h(page.body).gsub /\[\[[^\]]+\]\]/ do |link|
       title = link[2..-3]
-      if Page.find_by_title(title).nil?
-        link_to title, new_page_path(:title=>title)
-      else
-        link_to title, page_path(title)
-      end
+      link_to title, Page.find_by_title(title) || new_page_path(:title=>title)
     end
     textilize(body)
   end
