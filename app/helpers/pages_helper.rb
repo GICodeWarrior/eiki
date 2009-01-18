8c983f1c3cdc85d112bd0ea5afbf33f93434bb52
Rusty Burchfield <rusty@gicodelt.csh.rit.edu>
2009-01-18 00:10:31 -0800

    Textilize the body.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   def render_body(page)
-    h(page.body).gsub /\[\[[^\]]+\]\]/ do |link|
+    body = h(page.body).gsub /\[\[[^\]]+\]\]/ do |link|
       title = link[2..-3]
       if Page.find_by_title(title).nil?
         link_to title, new_page_path(:title=>title)
@@ -8,5 +8,6 @@ module PagesHelper
         link_to title, page_path(title)
       end
     end
+    textilize(body)
   end
 end
